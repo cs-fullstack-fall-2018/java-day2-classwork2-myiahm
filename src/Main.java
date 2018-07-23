52534d2b907e89670a3a48b5ce0da689624d19c3
Myiah Moore <myiahsade@gmail.com>
2018-07-23 15:30:32 -0500

    check in

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+  public static void main(String[] args) {
+      anything();
+}
+
+    public static void anything() {
+        int counter = 0;
+        while (counter < 5) {
+            counter += 1;
+            System.out.println(counter);
+        }
+    }
+}
---
+++ src/ex12.java
@@ -0,0 +1,17 @@
+public class ex12 {
+
+    public static void main(String[] args) {
+        anything();
+    }
+
+    public static void anything() {
+        int counter = 0;
+        while (counter < 5) {
+            counter += 1;
+            System.out.println(counter);
+            System.out.println(counter);
+            System.out.println(counter);
+
+        }
+    }
+}
